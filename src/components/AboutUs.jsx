102e405182749400b71e495945b12f87f47b9c79
Kalitha <kalithad@mobitel.lk>
2022-08-11 11:39:46 +0530

    new

---
+++ src/components/AboutUs.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+
+export default class AboutUs extends Component {
+  render() {
+    return (
+      <div>
+        <br/>
+        <h1>&nbsp;&nbsp;About Us</h1>
+        <div style={{marginTop:'400px'}}></div>
+        <br/>
+      </div>
+    )
+  }
+}
